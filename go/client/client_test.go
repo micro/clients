e80180ffb0c43e14930d1f406c4b3d690e808068
Janos Dobronszki <dobronszki@gmail.com>
2020-03-05 10:23:06 +0100

    Readme edits and test changes

--- go/client/client_test.go
+++ go/client/client_test.go
@@ -2,18 +2,21 @@ package client
 
 import "testing"
 
+type req struct {
+	Name string `json:"name"`
+}
+
+type rsp struct {
+	Msg string `json:"msg"`
+}
+
 func TestBasicCall(t *testing.T) {
-	response := map[string]string{}
-	if err := NewClient(nil).Call("go.micro.srv.greeter", "Say.Hello", map[string]string{"name": "John"}, &response); err != nil {
+	response := rsp{}
+	if err := NewClient(nil).Call("go.micro.srv.greeter", "Say.Hello", req{Name: "John"}, &response); err != nil {
 		t.Fail()
 	}
-	msg, ok := response["msg"]
-	if !ok {
-		t.Logf("Field message can't be found in response %v", response)
-		t.Fail()
-	}
-	if msg != "Hello John" {
-		t.Logf("Message is not as expected: %v", msg)
+	if response.Msg != "Hello John" {
+		t.Logf("Message is not as expected: %v", response.Msg)
 		t.Fail()
 	}
 }
