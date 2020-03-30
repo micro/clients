177055ce96d1a47618b8584a33782e70910735fd
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-03-31 00:40:47 +0300

    update proto from go-micro (#18)
    add daily sync proto from go-micro and regenerate for languages
    
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

---
+++ .github/sync.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+SRC_DIR="$1"
+DST_DIR="$2"
+
+if [ "x${SRC_DIR}" = "x" ]; then
+  echo "missing src dir";
+  exit 1;
+fi
+if [ "x${DST_DIR}" = "x" ]; then
+  echo "missing dst dir";
+  exit 1;
+fi
+
+mkdir -p $DST_DIR
+
+CORE_DIRS="broker client registry router runtime store auth network"
+
+for CORE_DIR in $CORE_DIRS; do
+  FILES=$(find $SRC_DIR/$CORE_DIR -type f -name "*.proto" -a ! -name "test.proto");
+  # | sed 's|service/proto/||g' | sed 's|proto/||g');
+  for SRC_FILE in $FILES; do
+    DST_FILE=$(echo $SRC_FILE | sed "s|$SRC_DIR/||g" | sed "s|service/proto/||g")
+    mkdir -p $DST_DIR/$(dirname $DST_FILE);
+    cp $SRC_FILE $DST_DIR/$DST_FILE;
+    sed -i 's|service/proto/||g' $DST_DIR/$DST_FILE;
+    sed -i '/github.com\/micro\/go-micro\/v2\//d' $DST_DIR/$DST_FILE;
+  done
+done
