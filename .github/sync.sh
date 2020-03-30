8f250c5c950cfe53b2b94233cad0206a7cbae394
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-03-30 17:34:49 +0300

    add daily sync workflow
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

---
+++ .github/sync.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+SRC_DIR="$1"
+DST_DIR="$2"
+
+if [ "x${SRC_DIR}" = "x" ]; then
+  echo "missing src dir";
+  exit 1;
+fi
+if [ "x${DST_DIR}" = "x" ]; then
+  echo "missing dst dir";
+  exit 1;
+fi
+
+mkdir -p $DST_DIR
+
+CORE_DIRS="broker client registry router runtime store auth"
+
+for CORE_DIR in $CORE_DIRS; do
+  FILES=$(find $SRC_DIR/$CORE_DIR -type f -name "*.proto" -a ! -name "test.proto");
+  # | sed 's|service/proto/||g' | sed 's|proto/||g');
+  for SRC_FILE in $FILES; do
+    DST_FILE=$(echo $SRC_FILE | sed "s|$SRC_DIR/||g" | sed "s|service/proto/||g")
+    mkdir -p $DST_DIR/$(dirname $DST_FILE);
+    cp $SRC_FILE $DST_DIR/$DST_FILE;
+  done
+done
