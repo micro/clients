a6daadff0df6247ca9cddc888d1d404fb99a34f0
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-04-10 16:49:57 +0300

    fix build
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
 
+### UBUNTU BIONIC ###
 sudo apt update
 sudo apt install -y protobuf-compiler
-sudo apt install -y --no-install-recommends python3 python3-pip python3-setuptools python3-dev
-# debian stuff python3-grpcio python-protobuf python3-grpc-tools
+sudo apt install -y --no-install-recommends python python-pip python-setuptools python-dev python-grpcio python-protobuf python-grpc-tools
 sudo apt install -y --no-install-recommends nodejs npm
 sudo apt install -y --no-install-recommends ruby
 sudo gem install grpc grpc-tools
-pip3 install --no-cache-dir --ignore-installed grpcio grpcio-tools
+#pip3 install --no-cache-dir --ignore-installed grpcio grpcio-tools
 #pip3 install --no-cache-dir grpcio-tools
 npm i grpc-tools
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
