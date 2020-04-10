505224b22c954b611c0080f4361640c1416cba04
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-04-10 16:56:34 +0300

    try to use external repo
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -e
 
 ### UBUNTU BIONIC ###
+echo "deb https://packages.le-vert.net/tensorflow/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
+wget -O - https://packages.le-vert.net/packages.le-vert.net.gpg.key | sudo apt-key add -
+
 sudo apt update
 sudo apt install -y protobuf-compiler
-sudo apt install -y --no-install-recommends python python-pip python-setuptools python-dev python-grpcio python-protobuf python-grpc-tools
+sudo apt install -y --no-install-recommends python3 python3-pip python3-setuptools python3-dev python3-grpcio python3-protobuf python3-grpc-tools
 sudo apt install -y --no-install-recommends nodejs npm
 sudo apt install -y --no-install-recommends ruby
 sudo gem install grpc grpc-tools
