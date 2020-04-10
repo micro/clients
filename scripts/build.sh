38ce0aff14e1572f314eba22d18cdb9bb112d5ef
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-04-10 17:03:50 +0300

    try to use external repo
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,12 +6,12 @@ wget -O - https://packages.le-vert.net/packages.le-vert.net.gpg.key | sudo apt-k
 
 sudo apt update
 sudo apt install -y protobuf-compiler
-sudo apt install -y --no-install-recommends python3 python3-pip python3-setuptools python3-dev python3-grpcio python3-protobuf python3-grpc-tools
+sudo apt install -y --no-install-recommends python3 python3-pip python3-setuptools python3-dev python3-grpcio python3-protobuf
 sudo apt install -y --no-install-recommends nodejs npm
 sudo apt install -y --no-install-recommends ruby
 sudo gem install grpc grpc-tools
 #pip3 install --no-cache-dir --ignore-installed grpcio grpcio-tools
-#pip3 install --no-cache-dir grpcio-tools
+pip3 install --no-cache-dir grpcio-tools
 npm i grpc-tools
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 sudo cargo install protobuf-codegen cargo install grpc-compiler
