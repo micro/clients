1e0dc0990258922ef5e9f745e22036f99aab155b
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-04-10 17:28:41 +0300

    try to use external repo
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,6 +14,7 @@ sudo gem install grpc grpc-tools
 pip3 install --no-cache-dir grpcio-tools
 npm i grpc-tools
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
 sudo cargo install protobuf-codegen cargo install grpc-compiler
 go mod init clients
 GOBIN=$(go env GOPATH)/bin go get github.com/unistack-org/protoc-gen-all github.com/golang/protobuf/protoc-gen-go@v1.3.4 github.com/micro/protoc-gen-micro/v2 github.com/micro/go-micro/v2@master
