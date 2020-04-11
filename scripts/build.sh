bd15797734f57beb0e66b7249e41b7100625f47c
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-04-11 03:24:27 +0300

    test
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,6 +13,6 @@ sudo apt install -y protobuf-compiler
 #pip3 install --no-cache-dir grpcio-tools
 #npm i grpc-tools
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-sudo cargo install protobuf-codegen grpc-compiler
+cargo install protobuf-codegen grpc-compiler
 go mod init clients
 GOBIN=$(go env GOPATH)/bin go get github.com/unistack-org/protoc-gen-all github.com/golang/protobuf/protoc-gen-go@v1.3.4 github.com/micro/protoc-gen-micro/v2 github.com/micro/go-micro/v2@master
