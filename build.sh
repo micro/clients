e99a25e05d922627b16b3324632fa2a0416faa72
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-05-25 14:24:14 +0300

    fix deps
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- build.sh
+++ build.sh
@@ -15,4 +15,4 @@ npm i grpc-tools
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo install protobuf-codegen grpc-compiler
 go mod init clients
-GOBIN=$(go env GOPATH)/bin go get github.com/unistack-org/protoc-gen-all github.com/golang/protobuf/protoc-gen-go@v1.3.5 github.com/micro/protoc-gen-micro/v2 github.com/micro/go-micro/v2@master
+GOBIN=$(go env GOPATH)/bin go get github.com/unistack-org/protoc-gen-all github.com/golang/protobuf/protoc-gen-go@v1.4.1 github.com/micro/protoc-gen-micro/v2 github.com/micro/go-micro/v2@latest
