3c19150dbbbb121d2b412102199a2a433cd1c8e2
Asim Aslam <asim@aslam.me>
2020-09-08 22:11:41 +0100

    Update

--- build.sh
+++ build.sh
@@ -15,4 +15,8 @@ npm i grpc-tools
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo install protobuf-codegen grpc-compiler
 go mod init clients
-GOBIN=$(go env GOPATH)/bin go get github.com/unistack-org/protoc-gen-all github.com/golang/protobuf/protoc-gen-go@v1.4.1 github.com/micro/protoc-gen-micro/v2 github.com/micro/go-micro/v2@latest
+cd cmd/protoc-gen-client && GOBIN=$(go env GOPATH)/bin go get ./...
+GOBIN=$(go env GOPATH)/bin go get \
+github.com/golang/protobuf/protoc-gen-go@v1.4.1 \
+github.com/micro/micro/v3/cmd/protoc-gen-micro \
+github.com/micro/micro/v3@latest
