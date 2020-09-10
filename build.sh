9d308159d2984e3641af3436ddf9dd861f992edc
Asim Aslam <asim@aslam.me>
2020-09-10 17:05:40 +0100

    clone source

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ sudo apt install -y protobuf-compiler
 sudo apt install -y --no-install-recommends python3 python3-pip python3-setuptools python3-dev python3-grpcio python3-protobuf
 sudo apt install -y --no-install-recommends nodejs npm
 sudo apt install -y --no-install-recommends ruby ruby-dev
+sudo apt install -y --no-install-recommends git-all
 sudo gem update --system
 sudo gem install grpc grpc-tools
 pip3 install --no-cache-dir grpcio-tools
@@ -22,5 +23,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo install protobuf-codegen grpc-compiler
 go mod init clients
 cd cmd/protoc-gen-client && go get ./...
+cd ..
 go get github.com/golang/protobuf/protoc-gen-go@v1.4.2
-go get -d github.com/micro/micro/v3@latest
+mkdir -p $GO_PATH/src/github.com/micro
+git clone https://github.com/micro/micro $GO_PATH/src/github.com/micro/micro 
--- generate.sh
+++ generate.sh
@@ -5,6 +5,6 @@ set -x
 rm -rf sdk
 GO_BIN=$(go env GOPATH)/bin
 GO_PATH=$(go env GOPATH)
-PROTO_DIR=$GO_PATH/pkg/mod/github.com/micro/micro/proto/
+PROTO_DIR=$GO_PATH/src/github.com/micro/micro/proto/
 PATH=$PATH:$GO_BIN:$(npm bin):/usr/local/bin/ protoc-gen-client -srcdir $PROTO_DIR -dstdir sdk/ -langs go,python,java,ruby,js,rust
 rm -rf go.mod go.sum node_modules/ package-lock.json
